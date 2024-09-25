fb3999171590604c492bf88dfb2a2052e2626a27
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-25 10:03:09 -0700

    More lines added

--- script.js
+++ script.js
@@ -10,12 +10,36 @@ mongoose.connect("mongodb://localhost/testdb")
   });
 
 
-  run()
+
+  //Function to create a new user and save it to the database
   async function run (){
-    const user =new User({name: "Kyle", age: 26})
-    await user.save()
-    console.log(user)
+
+    try {
+        const user = await new User(
+            {
+                name: "Kyle",
+                 age: 26,
+                 email: "Test@test.com",
+                hobbies: ["Weight Lifting", "Bowling"],
+                address: {
+                    street: "123 Main St",
+                },
+            
+            })
+    
+        console.log(user)
+        
+    } catch (e) {
+        console.log(e.message)
+
+        
+    }
+    
   }
+
+
+  //Call the function 
+  run()
 
 
   
--- user.js
+++ user.js
@@ -1,17 +1,48 @@
 //User schema file for mongodb
 
+//Importing mongoose
 const mongoose = require('mongoose');
 
+//Address schema
+const addressSchema =new mongoose.Schema({
+        street: String,
+        city : String
 
+
+      })
 
 
 //User schema (like a table in mongodb)
 const userSchema = new mongoose.Schema({ 
+    //List the user features with their data types.
     name: String,
-    age: Number 
+    age: {
+        type: Number,
+        min: 1,
+        max: 150
+    }, 
+    email: {
+        type: String,
+        //Validate the email input from the user
+        required: true,
+        lowercase: true
+    },
+    createdAt: {
+        type: Date,
+        immutable: true, //Cannot be changed
+        default: () => Date.now(),
+    } ,
+    updatedAt: {
+        type: Date,
+        default: () => Date.now(),
+    },
+    bestFriend: mongoose.SchemaTypes.ObjectId,
+    hobbies: [String],
+    address : addressSchema,
+    
 })
 
 
 //User model (Like a user collection in mongodb)
-
+//Exports the user model to be used in the controller
 module.exports = mongoose.model('User', userSchema)
